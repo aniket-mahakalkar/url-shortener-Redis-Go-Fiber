10c2b823cedff626c025930cc0f65d0832a766ca
Aniket Mahakalkar <mahakalkaraniket001@gmail.com>
2025-10-04 00:02:27 +0530

    rate limiting is added

--- api/routes/shorten.go
+++ api/routes/shorten.go
@@ -1,11 +1,16 @@
 package routes
 
 import (
+	"os"
+	"strconv"
 	"time"
 
+	"github.com/aniket-mahakalkar/shorten-url/database"
 	"github.com/aniket-mahakalkar/shorten-url/helpers"
 	"github.com/asaskevich/govalidator"
 	"github.com/gofiber/fiber/v2"
+	"github.com/google/uuid"
+	"github.com/redis/go-redis/v9"
 )
 
 type request struct {
@@ -38,6 +43,38 @@ func ShortenURL(c *fiber.Ctx) error {
 
 	}
 
+	// Rate Limiting
+
+	r2:= database.CreateClient(1)
+
+	defer r2.Close()
+
+	val, err := r2.Get(database.Ctx, c.IP()).Result()
+
+	if err == redis.Nil{
+
+		_ = r2.Set(database.Ctx, c.IP(), os.Getenv("API_QUOTA"), 30*60*time.Second).Err()
+	} else {
+
+		val , _ := r2.Get(database.Ctx, c.IP().Result())
+
+		valInt, _ := strconv.Atoi(val)
+
+		if valInt <= 0 {
+
+			limit, _ := r2.TTL(database.Ctx, c.IP()).Result()
+
+			return  c.Status(fiber.StatusServiceUnavailable).JSON(fiber.Map{
+
+				"error": "Rate Limit Exceeded",
+				"rate_limit_reset": limit / time.Nanosecond / time.Minute,
+
+			})
+
+		}
+		
+	}
+
 	if !govalidator.IsURL(body.URL){
 
 		return  c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"error":"Invaldi URL"})
@@ -55,13 +92,52 @@ func ShortenURL(c *fiber.Ctx) error {
 
 	body.URL = helpers.EnforceHTTP(body.URL)
 
-	resp := response{
-		URL: body.URL,
-		CustomShort: body.CustomShort,
-		Expiry: body.Expiry,
+	var id string
+
+	if body.CustomShort == ""{
+
+		id = uuid.New().String()[:6]
+
+	}else{
+
+		id = body.CustomShort
+
+	}
+
+	r := database.CreateClient(0)
+
+	defer r.Close()
+
+
+	var, _ = r.Get(database.Ctx, id).Result()
+
+	if val != "" {
+
+		return c.Status(fiber.StatusForbidden).JSON(fiber.Map{
+
+			"error" : "URL custom short is already is use"
+		}
+		)
+
+	}
+
+	if body.Expiry == 0 {
+
+		body.Expiry = 24
+	}
+
+
+	err = r.Set(database.Ctx, id, body.URL, body.Expiry*3600*time.Second).Err()
+
+	if err !=nil {
+
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"error": "Unable to connect to server",
+		})
+	}
+
 
 	
-	}
-	
-	return c.Status(fiber.StatusOK).JSON(resp)
+	r2.Decr(database.Ctx, c.IP())
+
 }
