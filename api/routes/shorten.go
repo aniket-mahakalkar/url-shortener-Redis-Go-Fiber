4d705961ed5f8996dc5a8f48aef2e7a22e80c9c9
Aniket Mahakalkar <mahakalkaraniket001@gmail.com>
2025-10-07 00:05:06 +0530

    docker file edited and routes fixed

--- api/routes/shorten.go
+++ api/routes/shorten.go
@@ -55,9 +55,7 @@ func ShortenURL(c *fiber.Ctx) error {
 
 		_ = r2.Set(database.Ctx, c.IP(), os.Getenv("API_QUOTA"), 30*60*time.Second).Err()
 	} else {
-
-		val , _ := r2.Get(database.Ctx, c.IP().Result())
-
+		
 		valInt, _ := strconv.Atoi(val)
 
 		if valInt <= 0 {
@@ -109,15 +107,14 @@ func ShortenURL(c *fiber.Ctx) error {
 	defer r.Close()
 
 
-	var, _ = r.Get(database.Ctx, id).Result()
+	val, _ = r.Get(database.Ctx, id).Result()
 
 	if val != "" {
 
 		return c.Status(fiber.StatusForbidden).JSON(fiber.Map{
 
-			"error" : "URL custom short is already is use"
-		}
-		)
+			"error" : "URL custom short is already is use",
+		})
 
 	}
 
@@ -137,7 +134,30 @@ func ShortenURL(c *fiber.Ctx) error {
 	}
 
 
+	resp := response{
+
+		URL: body.URL,
+		CustomShort: "",
+		Expiry: body.Expiry,
+		XRateRemaining: 10,
+		XRateLimitReset: 30,
+	}
+
+
 	
 	r2.Decr(database.Ctx, c.IP())
+
+	val , _ = r2.Get(database.Ctx, c.IP()).Result()
+
+	resp.XRateRemaining, _ = strconv.ParseInt(val, 10, 64)
+
+	ttl, _ := r2.TTL(database.Ctx, c.IP()).Result()
+
+	resp.XRateLimitReset = int64(ttl / time.Second)
+
+
+	resp.CustomShort = os.Getenv("DOMAIN") + "/" + id
+
+	return  c.Status(fiber.StatusOK).JSON(resp)
 
 }
